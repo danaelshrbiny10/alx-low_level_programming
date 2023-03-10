a86249ebcba1116697414496ff0b190a8094f18e
Dana <pythondjango66@gmail.com>
2023-03-10 22:42:15 +0200

    fix header

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
 
 /*a list that was given from the task 0 */
 
@@ -24,6 +21,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-
-#endif
