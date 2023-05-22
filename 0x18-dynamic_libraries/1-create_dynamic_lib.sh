5cb416824d06a99dd0d21a981f064c2aed73f17c
Dana <pythondjango66@gmail.com>
2023-05-23 00:11:27 +0300

    updated create file

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Get all the .c files in the current directory
-c_files=$(ls *.c 2>/dev/null)
-
-if [ -z "$c_files" ]; then
-  echo "No .c files found in the current directory."
-  exit 1
-fi
-
-# Compile the .c files into object files
-gcc -c -fPIC $c_files
-
-# Create the dynamic library
+gcc -c -Wall -Werror -fpic *.c
 gcc -shared -o liball.so *.o
-
-# Clean up the object files
-rm -f *.o
-
-echo "Dynamic library liball.so created successfully."
